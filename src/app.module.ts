d7685fbff88b6b7cd15231cadb4a0ccd957e024a
Jeremiah Olisa <jeremiaholisa453@gmail.com>
2025-04-17 22:07:36 +0100

    feat: add payment and subscription file setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { AppModule as KunsoleApp } from './app/app.module';
 import { PlanModule } from './plan/plan.module';
+import { SubscriptionModule } from './subscription/subscription.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { PlanModule } from './plan/plan.module';
     AuthModule,
     KunsoleApp,
     PlanModule,
+    SubscriptionModule,
+    PaymentModule,
   ],
   providers: [],
 })
